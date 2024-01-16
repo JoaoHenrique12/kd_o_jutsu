1b82be75616f77e811898e73b1a7df19654fecc1
Joao Henrique <joao.henrique1299@hotmail.com>
2024-01-16 09:45:06 -0300

    (feat) maintain jutsu_searched seals images on template search_jutsu_by_seals.

--- src/app.py
+++ src/app.py
@@ -18,19 +18,22 @@ def search_jutsu_by_name():
 @app.route("/search_jutsu_by_seals")
 def search_jutsu_by_seals():
     seals_names = list(map(get_seal_name_by_id, range(1,14)))
-    links = [url_for('static', filename=f'images/seals/{name}_seal.webp') for name in seals_names]
-    links = zip(links, seals_names)
+    main_images = [url_for('static', filename=f'images/seals/{name}_seal.webp') for name in seals_names]
+    main_images = zip(main_images, seals_names)
 
     search_query = request.args.get('search_query', '')
-    search_query = search_query[:-1]
+    search_query = search_query.rstrip().rstrip(',')
+
+    seals_names_found = search_query.split(',')
+
+    searched_images = [url_for('static', filename=f'images/seals/{name}_seal.webp') for name in seals_names_found]
+    searched_images = zip(searched_images, seals_names_found)
 
     seals_id = []
     if search_query != '':
-        seals_id = list(map(get_seal_id_by_name, search_query.split(',')))
-
-    print(seals_id)
+        seals_id = list(map(get_seal_id_by_name, seals_names_found))
 
     jutsus_found = search_jutsu_df_by_seals_id(seals_id)
     jutsus_found = append_seal_sequence(jutsus_found)
 
-    return render_template('search_jutsu_by_seals.html', image_links = links, jutsus = jutsus_found)
+    return render_template('search_jutsu_by_seals.html', main_images = main_images, searched_images = searched_images,  jutsus = jutsus_found)
